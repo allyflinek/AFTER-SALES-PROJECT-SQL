ec4305f7d405718342e4d8e45c89163ef5d869b2
allyf linek <allyf.linek@outlook.com>
2022-09-27 15:53:02 -0300

    Orcamento_DealerNet_V4_(ajuste_de_erros_de_codigo_digitacao)

--- [Camada0].[Pr_DealerNet_Orcamento].sql
+++ [Camada0].[Pr_DealerNet_Orcamento].sql
@@ -25,7 +25,7 @@ BEGIN
 	,oo.OficinaOrcamento_TipoOSCod                                AS Orcamento_TipoOSCod
 	,CONVERT(DATE,oo.OficinaOrcamento_Validade)                   AS Orcamento_Validade
 	,oo.OficinaOrcamento_Status                                   AS Orcamento_Status 
-	,oo.OficinaOracmento_KM                                       AS OficinaOrcamento_KM 
+	,oo.OficinaOrcamento_KM                                       AS OficinaOrcamento_KM 
 	,CONVERT(DATE,oo.OficinaOrcamento_ProximoContato)             AS Orcamento_ProximoContato
 	,oo.OficinaOrcamento_OficinaOrcamentoCodOrigem                AS Orcamento_OrcamentoCodOrigem
     ,oo.OficinaOrcamento_AtendimentoCod                           AS Orcamento_AtendimentoCod
@@ -33,19 +33,19 @@ BEGIN
 	,CONVERT(DATETIME,ooh.OficinaOrcamentoHistorico_Data)         AS Orcamento_HoraCriacao
 	,CONVERT(FLOAT, os.OficinaServico_Valor)                      AS OficinaServico_Valor
 	,CONVERT(FLOAT, op.OficinaProduto_Valor)                      AS OficinaProduto_Valor
-	,os.OficinaServico_OSCod                                      AS OficinaServico_OScod
+	,os.OficinaServico_OSCod                                     AS OficinaServico_OScod
 	,ooh.OficinaOrcamentoHistorico_UsuarioCod                     AS Orcamento_UsuarioCod
 	INTO camada0.DealerNet_Orcamento_teste
-	 FROM [Dealer].[DealerNetWF].dbo.OficinaOrcamento oo WITH (NOLOCK)
+	 FROM [Dealer].[DealerNetWF].dbo.OficinaOrcamento oo 
 	 LEFT JOIN (SELECT OficinaServico_OficinaOrcamentoCod
-	                 ,OficinaServico_Cod
+	                 ,OficinaServico_OSCod
 					 ,SUM(OficinaServico_Horas/CONVERT(FLOAT,3600) * OficinaServico_ValorUnitario) AS OficinaServico_Valor
 		        FROM[Dealer].[DealerNetWF].[dbo].OficinaServico 
-			    GROUP BY OficinaServico_OficinaOrcamentoCod, OficinaServico_Cod) os  WITH (NOLOCK) ON  os.OficinaServico_OficinaOrcamentoCod  = oo.OficinaOrcamento_Codigo
+			    GROUP BY OficinaServico_OficinaOrcamentoCod, OficinaServico_OSCod) os   ON  os.OficinaServico_OficinaOrcamentoCod  = oo.OficinaOrcamento_Codigo
 	 LEFT JOIN (SELECT OficinaProduto_OficinaOrcamentoCod
 	                  ,SUM(OficinaProduto_QtdePedida * OficinaProduto_ValorUnitario)               AS OficinaProduto_Valor
 		        FROM [Dealer].[DealerNetWF].[dbo].OficinaProduto 
-				GROUP BY OficinaProduto_OficinaOrcamentoCod) op                      WITH (NOLOCK) ON  op.OficinaProduto_OficinaOrcamentoCod  = oo.OficinaOrcamento_Codigo
-	 LEFT JOIN [Dealer].[DealerNetWF].[dbo].OficinaOrcamentoHistorico ooh            WITH (NOLOCK) ON ooh.OficinaOrcamento_Codigo             = oo.OficinaOrcamento_Codigo (WHERE oo.OficinaOrcamentoHistorico_Data >= @data and OficinaOrcamentoHistorico_Codigo = 'CRI')
+				GROUP BY OficinaProduto_OficinaOrcamentoCod) op                       ON  op.OficinaProduto_OficinaOrcamentoCod  = oo.OficinaOrcamento_Codigo
+	 LEFT JOIN [Dealer].[DealerNetWF].[dbo].OficinaOrcamentoHistorico ooh             ON  oo.OficinaOrcamento_Codigo             = ooh.OficinaOrcamento_Codigo            WHERE (ooh.OficinaOrcamentoHistorico_Data >= @data and ooh.OficinaOrcamentoHistorico_Codigo = 'CRI')
 	END 
 	GO

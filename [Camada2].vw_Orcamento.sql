244902be731694fd1a9e6b7cd4d3902cabfb764f
allyf linek <allyf.linek@outlook.com>
2022-09-28 10:45:31 -0300

    Orcamento_VW_V3_(versao_teste)

--- [Camada2].vw_Orcamento.sql
+++ [Camada2].vw_Orcamento.sql
@@ -1,14 +1,12 @@
 USE [stage]
 GO
 
-/****** Object:  View [camada2].[vw_Orcamento_Teste]    Script Date: 28/09/2022 10:37:20 ******/
+/****** Object:  View [camada2].[vw_Orcamento_Teste]    Script Date: 28/09/2022 10:37:20 *****hjj*/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
-
 
 ALTER VIEW [camada2].[vw_Orcamento_Teste]
 AS
@@ -23,8 +21,8 @@ SELECT [SISTEMA]
       ,[Observacao]
       ,[OS_Codigo]
       ,[TipoOS_Codigo]
-	  ,[Sigla_OS]
-	  ,[Descricao_OS]
+	,[Sigla_OS]
+	,[Descricao_OS]
       ,[Orcamento_Status]
       ,[KM]
       ,[Data_Validade]
@@ -37,12 +35,11 @@ SELECT [SISTEMA]
       ,[Produto_Valor]
       ,[Oficina_Valortotal]
       ,[Orcamento_Complementar]
---	  ,case when DATEDIFF(DAY, [DataCriacao], ISNULL([Data_Validade], GETDATE())) < 0 then 0 
---	   else DATEDIFF(DAY, [DataCriacao], ISNULL([Data_Validade], GETDATE())) end as TempoDuracao_Orcamento
+--	,case when DATEDIFF(DAY, [DataCriacao], ISNULL([Data_Validade], GETDATE())) < 0 then 0 
+--	 else DATEDIFF(DAY, [DataCriacao], ISNULL([Data_Validade], GETDATE())) end as TempoDuracao_Orcamento
 	  ,DATEDIFF(DAY, [DataCriacao],  GETDATE()) AS TempoDuracao_Orcamento
 	
   FROM [camada1].[vw_Orcamento_Teste]
-
 
 GO
 
